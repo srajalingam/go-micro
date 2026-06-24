5888f28205d93f807a85e2479eab94349239a7d0
srajalingam <rajalingam299@gmail.com>
2026-06-24 23:55:04 +0530

    first commit

---
+++ front-end/cmd/web/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		render(w, "test.page.gohtml")
+	})
+
+	fmt.Println("Starting front end service on port 8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func render(w http.ResponseWriter, t string) {
+
+	partials := []string{
+		filepath.Join("templates", "base.layout.gohtml"),
+		filepath.Join("templates", "header.partial.gohtml"),
+		filepath.Join("templates", "footer.partial.gohtml"),
+	}
+
+	var templateSlice []string
+	templateSlice = append(templateSlice, filepath.Join("templates", t))
+
+	for _, x := range partials {
+		templateSlice = append(templateSlice, x)
+	}
+
+	tmpl, err := template.ParseFiles(templateSlice...)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err := tmpl.Execute(w, nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
