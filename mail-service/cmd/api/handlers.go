dda48f31accc4b526e3f17fba8bf47dcc33d2b34
srajalingam <rajalingam299@gmail.com>
2026-07-02 00:00:20 +0530

    routes , handler, email template created

---
+++ mail-service/cmd/api/handlers.go
@@ -0,0 +1,35 @@
+package main
+
+import "net/http"
+
+func (app *Config) SendMail(w http.ResponseWriter, r *http.Request) {
+	type mailMessage struct {
+		From    string `json:"from"`
+		To      string `json:"to"`
+		Subject string `json:"subject"`
+		Message string `json:"message"`
+	}
+
+	var reqPayload mailMessage
+	err := app.readJSON(w, r, &reqPayload)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+	msg := Message{
+		From:    reqPayload.From,
+		To:      reqPayload.To,
+		Subject: reqPayload.Subject,
+		Data:    reqPayload.Message,
+	}
+	err = app.Mailer.SendSMTPMessage(msg)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+	payload := jsonResponse{
+		Error:   false,
+		Message: "Message sent to " + reqPayload.To + " successfully",
+	}
+	app.writeJSON(w, http.StatusOK, payload)
+}
